3ba0d2b5a5478dc203900c112d1d8827a82ba02a
Jay <joelogira@gmail.com>
2023-11-08 19:35:55 +0300

    Created loading component

---
+++ app/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+        <main className="text-center">
+            <h2 className="text-primary text-2xl">Loading...</h2>
+            <p>Not for long hopefully :)</p>
+        </main>
+    )
+}
